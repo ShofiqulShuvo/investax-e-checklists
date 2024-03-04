273aba3e2af32628bee8ac3b06867b81dd769ff4
Shofiqul Islam Shuvo <logic.shuvo@gmail.com>
2024-03-04 19:14:45 +0600

    otp page ready for review

--- js/index.js
+++ js/index.js
@@ -7,22 +7,27 @@ let currentStep = 1;
 
     function showStep(step) {
         const steps = document.querySelectorAll('.step');
-        for (let i = 0; i < steps.length; i++) {
-            steps[i].style.display = 'none';
+
+        if(steps.length > 0) {
+            
+            for (let i = 0; i < steps.length; i++) {
+                steps[i].style.display = 'none';
+            }
+    
+            steps[step - 1].style.display = 'block';
+    
+            if (step === totalSteps) {
+                nextBtn.style.display = 'none';
+                submitBtn.style.display = 'block';
+            } else {
+                nextBtn.style.display = 'block';
+                submitBtn.style.display = 'none';
+            }
+    
+            // Disable previous button on the first step
+            prevBtn.disabled = step === 1;
+
         }
-
-        steps[step - 1].style.display = 'block';
-
-        if (step === totalSteps) {
-            nextBtn.style.display = 'none';
-            submitBtn.style.display = 'block';
-        } else {
-            nextBtn.style.display = 'block';
-            submitBtn.style.display = 'none';
-        }
-
-        // Disable previous button on the first step
-        prevBtn.disabled = step === 1;
     }
 
     function nextStep() {
@@ -38,3 +43,79 @@ let currentStep = 1;
             showStep(currentStep);
         }
     }
+
+
+
+
+// for otp page
+const inputs = document.querySelectorAll(".otp-field > input");
+const otpSubmitButton = document.querySelector("#otp-submit");
+
+// Check if inputs and Button elements exist
+if (inputs.length > 0 && otpSubmitButton) {
+  window.addEventListener("load", () => inputs[0].focus());
+  otpSubmitButton.setAttribute("disabled", "disabled");
+
+  inputs[0].addEventListener("paste", function (event) {
+    event.preventDefault();
+
+    const pastedValue = (event.clipboardData || window.clipboardData).getData(
+      "text"
+    );
+    const otpLength = inputs.length;
+
+    for (let i = 0; i < otpLength; i++) {
+      if (i < pastedValue.length) {
+        inputs[i].value = pastedValue[i];
+        inputs[i].removeAttribute("disabled");
+        inputs[i].focus;
+      } else {
+        inputs[i].value = ""; // Clear any remaining inputs
+        inputs[i].focus;
+      }
+    }
+  });
+
+  inputs.forEach((input, index1) => {
+    input.addEventListener("keyup", (e) => {
+      const currentInput = input;
+      const nextInput = input.nextElementSibling;
+      const prevInput = input.previousElementSibling;
+
+      if (currentInput.value.length > 1) {
+        currentInput.value = "";
+        return;
+      }
+
+      if (
+        nextInput &&
+        nextInput.hasAttribute("disabled") &&
+        currentInput.value !== ""
+      ) {
+        nextInput.removeAttribute("disabled");
+        nextInput.focus();
+      }
+
+      if (e.key === "Backspace") {
+        inputs.forEach((input, index2) => {
+          if (index1 <= index2 && prevInput) {
+            input.setAttribute("disabled", true);
+            input.value = "";
+            prevInput.focus();
+          }
+        });
+      }
+
+      otpSubmitButton.classList.remove("active");
+      otpSubmitButton.setAttribute("disabled", "disabled");
+
+      const inputsNo = inputs.length;
+      if (!inputs[inputsNo - 1].disabled && inputs[inputsNo - 1].value !== "") {
+        otpSubmitButton.classList.add("active");
+        otpSubmitButton.removeAttribute("disabled");
+
+        return;
+      }
+    });
+  });
+} 
